6718aca30a06ec32fd39525916113c180a7c1429
tszkoda <55335305+tszkoda@users.noreply.github.com>
2024-04-02 14:10:20 -0400

    -added a Glass sword
    -changed the properties of the Sturdy Gem Sword

--- src/main/java/me/tszkoda/simpleswordmod/block/ModBlocks.java
+++ src/main/java/me/tszkoda/simpleswordmod/block/ModBlocks.java
@@ -34,7 +34,7 @@ public class ModBlocks {
 
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
                                                                             CreativeModeTab tab) {
-        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab).fireResistant()));
     }
 
     public static void register(IEventBus eventBus) {
--- src/main/java/me/tszkoda/simpleswordmod/item/ModItems.java
+++ src/main/java/me/tszkoda/simpleswordmod/item/ModItems.java
@@ -2,7 +2,9 @@ package me.tszkoda.simpleswordmod.item;
 
 import me.tszkoda.simpleswordmod.SimpleSwordMod;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,10 +16,14 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, SimpleSwordMod.MOD_ID);
 
     public static final RegistryObject<Item> STURDY_GEM = ITEMS.register("sturdy_gem",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SIMPLESWORD_TAB)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SIMPLESWORD_TAB).fireResistant()));
 
     public static final RegistryObject<Item> STURDY_GEM_SWORD = ITEMS.register("sturdy_gem_sword",
-            () -> new SwordItem(ModToolTiers.STURDY_GEM, 12, 12,
+            () -> new SwordItem(ModToolTiers.STURDY_GEM, 18, 12,
+                    new Item.Properties().tab(ModCreativeModeTab.SIMPLESWORD_TAB).fireResistant().rarity(Rarity.EPIC)));
+
+    public static final RegistryObject<Item> GLASS_SWORD = ITEMS.register("glass_sword",
+            () -> new SwordItem(Tiers.GOLD, 0, 0,
                     new Item.Properties().tab(ModCreativeModeTab.SIMPLESWORD_TAB)));
 
     public static void register(IEventBus eventBus) {
